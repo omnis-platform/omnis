3179bbaf37c8b6b6decafe5d0ee755aa1582de87
Igor Bezsmertnyi <mr.igor96@outlook.com>
2018-01-24 21:48:36 +0100

    fix require issue

--- omnis/config.js
+++ omnis/config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const configPath = path.join(process.cwd(), 'omnis.json')
-const config = require(configPath)
+const config = require('../../../omnis.json')
 
 module.exports = config
