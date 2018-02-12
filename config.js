436b6effd4a6a3bc3e6d5b2cea62b3c999a7383c
Igor Bezsmertnyi <mr.igor96@outlook.com>
2018-02-12 18:25:19 +0100

    change scope

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const path = require('path')
-const config = require('../../../omnis.json')
+const config = require('../../omnis.json')
 
 module.exports = config
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-module.exports = require('./omnis')
+const connect = require('./connect')
+const email = require('./email')
+const file = require('./file')
+const get = require('./get')
+const post = require('./post')
+const patch = require('./patch')
+const put = require('./put')
+const remove = require('./remove')
+const user = require('./user')
+const config = require('./config')
+const Omnis = {}
+
+Omnis.connect = connect
+Omnis.email = email
+Omnis.file = file
+Omnis.get = get
+Omnis.post = post
+Omnis.patch = patch
+Omnis.put = put
+Omnis.delete = remove
+Omnis.user = user
+Omnis.config = config
+
+module.exports = Omnis
